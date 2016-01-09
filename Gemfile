1b50d48cb9d0fdcf5b77bf95255550e906b1483d
Kris Steigerwald <kris.steigerwald@gmail.com>
2016-01-09 14:46:42 -0500

    - introduce highline for cli

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 # A sample Gemfile
 source "https://rubygems.org"
 gem 'mechanize'
+gem 'highline'
 # gem "rails"
--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@
 
 require 'rubygems'
 require 'mechanize'
+require 'highline'
+
+cli = HighLine.new
+user = cli.ask "Dominion Power User Name:"
+pass = cli.ask "Dominion Power Password:"
 
 a = Mechanize.new
 a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -9,8 +14,8 @@ a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 a.get('https://www.dom.com/residential/dominion-virginia-power/sign-in') do |page|
 
   profile_page = page.form_with(:action => '/residential/dominion-virginia-power/sign-in') do |f|
-    f.user = ARGV[0]
-    f.password = ARGV[1]
+    f.user = user
+    f.password = pass
   end.click_button
 
   puts profile_page.to_yaml
