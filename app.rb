32113a40ba2f160c42591828e084f63b8b280a9b
Kris Steigerwald <kris.steigerwald@gmail.com>
2016-01-09 14:39:41 -0500

    - login to web form

--- app.rb
+++ app.rb
@@ -3,4 +3,15 @@
 require 'rubygems'
 require 'mechanize'
 
+a = Mechanize.new
+a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
+a.get('https://www.dom.com/residential/dominion-virginia-power/sign-in') do |page|
+
+  profile_page = page.form_with(:action => '/residential/dominion-virginia-power/sign-in') do |f|
+    f.user = ARGV[0]
+    f.password = ARGV[1]
+  end.click_button
+
+  puts profile_page.to_yaml
+end
