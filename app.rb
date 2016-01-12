0b99dd8197cb13b79bd8fa0ae48937c784a79431
Kris Steigerwald <kris.steigerwald@gmail.com>
2016-01-12 12:20:25 -0500

    - working login for test

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ require 'mechanize'
 require 'highline'
 
 cli = HighLine.new
-user = cli.ask "Dominion Power User Name:"
-pass = cli.ask "Dominion Power Password:"
+user = cli.ask "Dominion Power User Name:" || ENV['DOM_USER']
+pass = cli.ask "Dominion Power Password:" || ENV['DOM_PASS']
 
 a = Mechanize.new
 a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -18,8 +18,8 @@ a.get('https://www.dom.com/residential/dominion-virginia-power/sign-in') do |pag
     f.password = pass
   end.click_button
 
-  billing_page = a.click(profile_page.link_with(:text => 'Billing'))
-
+  #billing_page = a.click(profile_page.link_with(:text => 'Billing'))
+  puts profile_page.links.to_yaml
   detailed_energy_usage_page = a.click(profile_page.link_with(text: 'Detailed Energy Usage')) do |p|
 
   end
--- test/test_app.rb
+++ test/test_app.rb
@@ -1,8 +1,46 @@
 require 'minitest/autorun'
+require 'mechanize'
 
 class TestApp < Minitest::Test
+  i_suck_and_my_tests_are_order_dependent!
+
+  def setup
+    @a = Mechanize.new do |agent|
+      agent.user_agent = (Mechanize::AGENT_ALIASES.keys - ['Mechanize']).sample
+      agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+    puts @a.user_agent
+
+    @login_page = @a.get('https://mya.dom.com/')
+    @links = ['/billing/billing', '/usage/dailyenergyusage']
+  end
+
+  def test_login_page
+
+    login_form = @login_page.forms.first #.form_with(:action => '/residential/dominion-virginia-power/sign-in')
+    login_form.field_with(name: 'USER').value = ENV['DOM_USER']
+    login_form.field_with(name: 'PASSWORD').value = ENV['DOM_PASS']
+
+    puts login_form.to_yaml
+
+    assert login_form.field_with(name: 'USER').class.eql?(Mechanize::Form::Text), "No input called 'user' found"
+    assert login_form.field_with(name: 'PASSWORD').class.eql?(Mechanize::Form::Field), "No input called 'password' found"
+
+    #@profile_page = login_form.submit(login_form.buttons.first) #@a.submit(login_form, login_form.buttons.first)
+    @a.submit(login_form, login_form.buttons.first)
+    @profile_page = @a.get('https://mya.dom.com/')
+    puts @profile_page.content
+
+    if @profile_page.body.include? 'Please try again'
+      puts "login failed"
+    else
+      puts @profile_page.search("*[text() = 'Please try again']").to_yaml
+    end
+  end
+
   def test_profile_page
-    puts "lasjfkas"
+    #refute_empty @profile_page.search("*[text() = 'Account Number:']"), "login failed"
+
   end
 end
 
